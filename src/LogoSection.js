28bbdace4b4aeb90096d3bf995604cb88fcae207
Anuja Patel <anuja.patel@preciousinfosystem.com>
2023-05-23 15:12:00 +0530

    Fourth commit

--- src/LogoSection.js
+++ src/LogoSection.js
@@ -16,7 +16,7 @@ const LogoSection = () => {
     },
     {
       id: 3,
-      src: "OYO Logo.png",
+      src: "Vector.png",
       alt: "OYO Logo",
     },
     {
@@ -31,21 +31,22 @@ const LogoSection = () => {
     },
     {
       id: 6,
-      src: "OLA Logo.png",
+      src: "OLA logo.png",
       alt: "Ola Logo",
     },
     {
       id: 7,
-      src: "OLA Logo.png",
+      src: "OLA logo.png",
       alt: "Ola Logo",
     },
     {
       id: 8,
-      src: "OLA Logo.png",
+      src: "OLA logo.png",
       alt: "Ola Logo",
     },
     // Add more logos here
   ];
+
 
   const [startX, setStartX] = useState(null);
   const [scrollLeft, setScrollLeft] = useState(0);
@@ -61,7 +62,8 @@ const LogoSection = () => {
     if (!startX) return;
     const x = e.pageX;
     const walk = (x - startX) * 2; // Adjust the sliding speed
-    containerRef.current.scrollLeft = scrollLeft - walk;
+    const logoContainer = containerRef.current;
+    logoContainer.scrollLeft = scrollLeft - walk;
   };
 
   const handleMouseUp = () => {
@@ -74,55 +76,93 @@ const LogoSection = () => {
     containerRef.current.scrollLeft -= delta;
   };
 
-  return (
-    <div
-      style={{
-        width: "100%",
-        maxWidth: "1440px",
 
-        height: "330px",
-        flexWrap: "wrap",
-        display: "flex",
-        flexDirection: "column",
-        paddingLeft: "20px",
-        position: "relative",
-        overflowX: "hidden", // Enable horizontal scrolling
-        overflowY: "hidden", // Disable vertical scrolling
-        margin: "0 auto", // Center the logo section horizontally
-        scrollbarWidth: "none", // Hide the scrollbar for Firefox
-        "-ms-overflow-style": "none", // Hide the scrollbar for IE and Edge
-        "&::-webkit-scrollbar": {
-          display: "none", // Hide the scrollbar for Chrome, Safari, and Opera
-        },
-      }}
-      ref={containerRef}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseUp}
-      onWheel={handleWheel}
-    >
-      <h3 style={{ marginTop: "28px", width: "686px", height: "24px" }}>
+  return (
+    <div style={{ maxWidth: "1440px", height: "300px", width: "100%", margin: "0 auto" }}>
+      <h3
+        style={{
+          marginTop: "28px",
+          width: "100%",
+          maxWidth: "686px",
+          height: "24px",
+          position: "fixed",
+          top: "0",
+          background: "white",
+          zIndex: "1",
+        }}
+      >
         Join these great companies committed to fighting climate change
       </h3>
       <div
         style={{
           display: "flex",
-
-          justifyContent: "center",
+          justifyContent: "flex-start",
           alignItems: "center",
-          //flexWrap: "wrap",
-          gap: "48px",
           marginTop: "60px",
+          overflowX: "hidden",
+          overflowY: "hidden",
+          scrollbarWidth: "none",
+          "-ms-overflow-style": "none",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
         }}
+        ref={containerRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
+        onWheel={handleWheel}
       >
+
         {logos.map((logo) => (
-          <div key={logo.id}>
-            <img
-              src={logo.src}
-              alt={logo.alt}
-              style={{ width: "170px", height: "96px", margin: "10px" }}
-            />
+          <div
+            key={logo.id}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+
+              alignItems: "center",
+              marginRight: "48px", // Added margin between boxes
+              marginTop: "128px", // Apply top margin
+              marginBottom: "76px",
+              background: "#FFFFFF", // Apply white background
+              boxShadow: "0px 30px 40px rgba(212, 217, 232, 0.2)", // Apply grey shadow
+              borderRadius: "16px", // Apply border radius
+              //display: "flex",
+              //flexDirection: "column",
+              justifyContent: "center",
+              //alignItems: "center",
+              padding: "16px", // Add padding for content
+            }}
+          >
+            <div
+              style={{
+                width: "170px",
+                height: "96px",
+                margin: "10px",
+                //gap:"48px",
+                //border: "1px solid #ccc",
+                borderRadius: "8px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+
+              }}
+            >
+              <img
+                src={logo.src}
+                alt={logo.alt}
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  width: "122px",
+                  height: "26",
+
+                }}
+              />
+            </div>
+            {/* <div style={{ marginTop: "6px" }}>{logo.alt}</div> */}
           </div>
         ))}
       </div>
